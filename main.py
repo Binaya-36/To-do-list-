663412c997407183b29191861b84370a7cee2ad4
Binaya Raj Shrestha <bnaystha6@gmail.com>
2025-08-05 20:45:20 +0545

    function to display the menu

---
+++ main.py
@@ -0,0 +1,29 @@
+#create an empty list to store the tasks and their status
+todo_list= []
+
+# function to display the menu
+def menu():
+    while(True):
+        print("**Menu**")
+        print("1,Add a new task")
+        print("2,View all the tasks")
+        print("3,Remove the tasks")
+        print("4,Mark a task as completed")
+        print("5,Exit")
+
+        choice=input("What you want to do?")
+        if(choice=="1"):
+            add_task()
+        elif(choice=="2"):
+            view_task()
+        elif(choice=="3"):
+            remove_task()
+        elif(choice=="4"):
+            mark_done()
+        elif(choice=="5"):
+            exit()
+        else:
+            print("Wrong choice")
+menu()
+
+
