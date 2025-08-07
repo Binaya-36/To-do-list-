76a21333585bcd5e2c5205eba3efd419a3bec1a3
Binaya Raj Shrestha <bnaystha6@gmail.com>
2025-08-07 21:27:56 +0545

    function to view the task in the list

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 #create an empty list to store the tasks and their status
 todo_list= []
+
 #function to add task to the todo list
 def add_task():
-    task=input("Enter the task:")
+    task=input("\nEnter the task:")
     todo_list.append({"Task":task,"Status":"Pending"})
-    print("Task added succesfully")
+    print("Task added succesfully\n")
 
+#function to view the task
+def view_task():
+            if(len(todo_list)==0):
+                print("No task available")
+            else:
+                for index,task in enumerate(todo_list,start=1):
+                  print(f"{index:}.{task['Task']}-{task['Status']}")
+        
+        
 
 # function to display the menu
 def menu():
     while(True):
         print("**Menu**")
-        print("1,Add a new task")
-        print("2,View all the tasks")
-        print("3,Remove the tasks")
-        print("4,Mark a task as completed")
-        print("5,Exit")
+        print("1.Add a new task")
+        print("2.View all the tasks")
+        print("3.Remove the tasks")
+        print("4.Mark a task as completed")
+        print("5.Exit")
 
-        choice=input("What you want to do?")
+        choice=input("What you want to do?\n")
         if(choice=="1"):
             add_task()
         elif(choice=="2"):
@@ -27,9 +37,9 @@ def menu():
         elif(choice=="4"):
             mark_done()
         elif(choice=="5"):
-            exit()
+            exit_button()
         else:
             print("Wrong choice")
-menu()
 
+menu()
 
